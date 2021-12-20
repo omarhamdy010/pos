cf2ee04b51e3a6694ed567f3e51a5beb7af2963d
omarhamdy010 <hamdyomar065@gmail.com>
2021-12-20 14:07:02 +0200

    lang and configration admin lte

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,59 @@
+{{--@include('layouts.dashboard.app')--}}
+{{--@section('content')--}}
+{{--@dd('okoioiidfikhgio[[[h sdasdh sdfahsea')--}}
+
+{{--@endsection--}}
+@extends('layouts.dashboard.app')
+
+@section('content')
+
+    <div class="content-wrapper">
+
+        <section class="content-header">
+
+            <h1>Blank page
+                    <small>start from here</small>
+            </h1>
+
+            <ol class="breadcrumb">
+                <li class="active"><i class="fa fa-dashboard"></i>{{__('site.dashboard')}}</li>
+            </ol>
+        </section>
+
+        <section class="content">
+
+
+        </section><!-- end of content -->
+
+    </div><!-- end of content wrapper -->
+
+
+@endsection
+
+{{--@push('scripts')--}}
+
+{{--    <script>--}}
+
+{{--        //line chart--}}
+{{--        var line = new Morris.Line({--}}
+{{--            element: 'line-chart',--}}
+{{--            resize: true,--}}
+{{--            data: [--}}
+{{--                    @foreach ($sales_data as $data)--}}
+{{--                {--}}
+{{--                    ym: "{{ $data->year }}-{{ $data->month }}", sum: "{{ $data->sum }}"--}}
+{{--                },--}}
+{{--                @endforeach--}}
+{{--            ],--}}
+{{--            xkey: 'ym',--}}
+{{--            ykeys: ['sum'],--}}
+{{--            labels: ['@lang('site.total')'],--}}
+{{--            lineWidth: 2,--}}
+{{--            hideHover: 'auto',--}}
+{{--            gridStrokeWidth: 0.4,--}}
+{{--            pointSize: 4,--}}
+{{--            gridTextFamily: 'Open Sans',--}}
+{{--            gridTextSize: 10--}}
+{{--        });--}}
+{{--    </script>--}}
+{{--@endpush--}}
---
+++ routes/dashboard/web.php
@@ -0,0 +1,12 @@
+<?php
+
+Route::group(
+    ['prefix' => LaravelLocalization::setLocale(), 'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]], function(){
+
+        Route::prefix('dashboard')->name('dashboard.')->group(function (){
+
+            Route::get('/index','DashboardController@index')->name('index');
+
+        });
+});
+
